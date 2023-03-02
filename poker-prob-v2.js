4258f5ab81ea6b0315bdf1a099d0d7650dfe2cb9
somkene <somkenemuscle@gmail.com>
2023-03-02 22:07:18 +0100

    first logic

--- poker-prob-v2.js
+++ poker-prob-v2.js
@@ -1,13 +1,27 @@
 const NUM_TRIALS = 3;
+const suits = ['H', 'D', 'C', 'S'];
+const numbers = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
 
+function getCards() {
+let drawnCards = []
+  for (let i = 0; i< suits.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+    let cards = suits[i] + numbers[j]
+    drawnCards.push(cards)
+    }
+  }
+  return drawnCards
+}
+let drawnCard = getCards()
+console.log(drawnCard)
 /**
  * returns a card as a number, 0 -- 51.
  * 0 as SA, 1 as HA, 2 as DA, 3 as CA, 4 as SK, ...
  */
 function drawACard() {
-  return Math.floor(Math.random() * 52);
+  return Math.floor(Math.random() * drawnCard.length);
 }
-
+console.log(drawACard)
 /**
  * returns an array of cards, i.e., an array of five 0-51 integers.
  * [0, 4, 8, 12, 16] indicates [SA, SK, SQ, SJ, ST]
